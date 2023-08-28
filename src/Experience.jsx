94387b053bd325f5e21b97c87ede2e75a55ea159
venkateshwebdev <nikhilsirigineedi3@gmail.com>
2023-08-28 23:55:28 +0530

    text to html

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -28,11 +28,16 @@ const Experience = () => {
     </mesh>}
     {startGame&&
     <mesh>
-    <Text position={[0,1,-100]} color={"red"}>Find the Yellow Text in this scene.</Text>
+    {/* <Text position={[0,1,-100]} color={"red"}>Find the Yellow Text in this scene.</Text> */}
+    <Html position={[0,1,-100]} color={"red"}>Find the Yellow Text in this scene.</Html>
     <Text position={[0,1,-20]}>Go staright you will find a Red Text.</Text>
-    <Text position={[-100,1,-100]} color={"yellow"}>Ok One more , on the Right you will Find The Pink Box.</Text> 
+    <Html position={[0,1,-20]}>Go staright you will find a Red Text.</Html>
+    {/* <Text position={[-100,1,-100]} color={"yellow"}>Ok One more , on the Right you will Find The Pink Box.</Text>  */}
+    <Html position={[-100,1,-100]} color={"yellow"}>Ok One more , on the Right you will Find The Pink Box.</Html> 
     <mesh position={[-130,1,-130]} scale={2} onClick={handleClick}>
-      <Text style={{width:"300px"}} color="pink" position-y={1}>{!message?"Clik on this Block":"You Can go to the village Now find the village. Press the arrowDown button for 20 sec and go right"} </Text>
+      {/* <Text style={{width:"300px"}} color="pink" position-y={1}>{!message?"Clik on this Block":"You Can go to the village Now find the village. Press the arrowDown button for 20 sec and go right"} </Text> */}
+      <Html style={{width:"300px"}} color="pink" position-y={1}>{!message?"Clik on this Block":"You Can go to the village Now find the village. Press the arrowDown button for 20 sec and go right"} </Html>
+
       <boxGeometry />
       <meshBasicMaterial color={"red"} />
       </mesh>
