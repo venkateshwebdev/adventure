0752f7169cd4c46ba32db6a721b8c8e786819e20
venkateshwebdev <nikhilsirigineedi3@gmail.com>
2023-08-29 00:07:28 +0530

    text to html

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -14,8 +14,7 @@ const Experience = () => {
     <>
     {/* <fog attach="fog" args={["black",1,20]} />  */}
     <ambientLight intensity={1} />
-    {/* <Stars  /> */}
-    {/* <Environment preset="city" /> */}
+    <Sky  />
     <directionalLight position={[1,5,5]} intensity={10} castShadow />
     <ScrollControls pages={3}>
       <Village />
@@ -23,21 +22,21 @@ const Experience = () => {
     {!startGame&&
     <mesh position={[0,1,0]} onClick={()=>setStartGame(true)}>
     {/* <Text position-y={1}>Click this box to start the Game</Text> */}
-    <Html position-y={1}>Click this box to start the Game</Html>
+    <Html center style={{fontSize:"90px",width:"1000px"}} position-y={1}>Click this box to start the Game</Html>
     <boxGeometry/>
     <meshNormalMaterial />
     </mesh>}
     {startGame&&
     <mesh>
     {/* <Text position={[0,1,-100]} color={"red"}>Find the Yellow Text in this scene.</Text> */}
-    <Html position={[0,1,-100]} color={"red"}>Find the Yellow Text in this scene.</Html>
+    <Html position={[0,1,-100]} center style={{fontSize:"90px",width:"1000px"}} color={"red"}>Find the Yellow Text in this scene.</Html>
     {/* <Text position={[0,1,-20]}>Go staright you will find a Red Text.</Text> */}
-    <Html position={[0,1,-20]}>Go staright you will find a Red Text.</Html>
+    <Html position={[0,1,-20]} center style={{fontSize:"90px",width:"1000px",color:"white"}}>Go staright you will find a Red Text.</Html>
     {/* <Text position={[-100,1,-100]} color={"yellow"}>Ok One more , on the Right you will Find The Pink Box.</Text>  */}
-    <Html position={[-100,1,-100]} color={"yellow"}>Ok One more , on the Right you will Find The Pink Box.</Html> 
+    <Html position={[-100,1,-100]} center style={{fontSize:"90px",width:"1000px",color:"yellow"}}>Ok One more , on the Right you will Find The Pink Box.</Html> 
     <mesh position={[-130,1,-130]} scale={2} onClick={handleClick}>
       {/* <Text style={{width:"300px"}} color="pink" position-y={1}>{!message?"Clik on this Block":"You Can go to the village Now find the village. Press the arrowDown button for 20 sec and go right"} </Text> */}
-      <Html style={{width:"300px"}} color="pink" position-y={1}>{!message?"Clik on this Block":"You Can go to the village Now find the village. Press the arrowDown button for 20 sec and go right"} </Html>
+      <Html center style={{fontSize:"90px",width:"1000px",color:"pink"}} position-y={1}>{!message?"Clik on this Block":"You Can go to the village Now find the village. Press the arrowDown button for 20 sec and go right"} </Html>
 
       <boxGeometry />
       <meshBasicMaterial color={"red"} />
@@ -45,7 +44,7 @@ const Experience = () => {
     </mesh>}
 
     <mesh position={[0,0,-1]} scale={2000} rotation-x={-Math.PI/2} receiveShadow>
-      <planeGeometry/>
+      <planeGeometry receiveShadow/>
       <MeshReflectorMaterial color={"#000000"} receiveShadow/>
     </mesh>
     </ScrollControls>
