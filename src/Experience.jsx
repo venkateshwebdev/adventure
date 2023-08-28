d2b4861d9888cc7ac84f11264de9359b841ba194
venkateshwebdev <nikhilsirigineedi3@gmail.com>
2023-08-29 00:26:35 +0530

    text to html

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,4 @@
-import {OrbitControls,Sky,useGLTF,useAnimations,Scroll,useScroll, PivotControls, Environment, Text, Float, MeshReflectorMaterial, ScrollControls, SoftShadows, AccumulativeShadows, Stars, Cloud, Html} from "@react-three/drei"
+import {OrbitControls,Sky,useGLTF,useAnimations,Scroll,useScroll, PivotControls, Environment, Text, Float, MeshReflectorMaterial, ScrollControls, SoftShadows, AccumulativeShadows, Stars, Cloud, Html, useTexture} from "@react-three/drei"
 import { useEffect, useRef, useState } from "react";
 import {easing} from "maath"
 import { useFrame } from "@react-three/fiber";
@@ -10,6 +10,9 @@ const Experience = () => {
   const handleClick =()=>{
     setMeassge(prev=>!prev)
   }
+  const aoTexture = useTexture("/3d/textures/park_sand_ao_1k.png");
+  const disp = useTexture("/3d/textures/park_sand_disp_1k.png")
+  const roughMap = useTexture("/3d/textures/park_sand_rough_1k.png")
   return (
     <>
     {/* <fog attach="fog" args={["black",1,20]} />  */}
@@ -45,8 +48,8 @@ const Experience = () => {
 
     <mesh position={[0,0,-1]} scale={2000} rotation-x={-Math.PI/2} receiveShadow>
       <planeGeometry receiveShadow/>
-      <MeshReflectorMaterial color={"#000000"} receiveShadow/>
-    </mesh>
+      <meshStandardMaterial receiveShadow color={"#C2B280"}  map={aoTexture} roughnessMap={roughMap} />
+    </mesh> 
     </ScrollControls>
     </>
   );
